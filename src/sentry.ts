8e382d4bfad8733b39292a65c5078742e125dba4
makkelie_dev <makkelie@protonmail.com>
2025-03-06 21:20:35 +0100

    Added sentry. Upgraded node

---
+++ src/sentry.ts
@@ -0,0 +1,13 @@
+import * as Sentry from "@sentry/react";
+
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+Sentry.init({
+  dsn: "https://76b39e9e4a5ab1c3b2dddc14ac9638e3@o4508927143444480.ingest.us.sentry.io/4508927145213952",
+  integrations: [Sentry.reactRouterV5BrowserTracingIntegration({ history })],
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
